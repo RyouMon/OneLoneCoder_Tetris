ad556d26d25ffe55fce52d5c7ab65c4860effffa
Wen <wenslife@outlook.com>
2023-07-19 22:07:26 +0800

    Add project files.

---
+++ OneLoneCoder_Tetris.cpp
@@ -0,0 +1,12 @@
+﻿// OneLoneCoder_Tetris.cpp : Defines the entry point for the application.
+//
+
+#include "OneLoneCoder_Tetris.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
