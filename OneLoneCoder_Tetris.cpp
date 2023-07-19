f863356034a82253a6f0bf4da7debcd8a70cdf90
Wen <wenslife@outlook.com>
2023-07-19 22:54:36 +0800

    定义Assets：方块

--- OneLoneCoder_Tetris.cpp
+++ OneLoneCoder_Tetris.cpp
@@ -1,12 +1,18 @@
 ﻿// OneLoneCoder_Tetris.cpp : Defines the entry point for the application.
 //
 
-#include "OneLoneCoder_Tetris.h"
-
+#include <iostream>
 using namespace std;
+
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	wstring tetromino[7];
+	tetromino[0].append(L"..X...X...X...X.");
+	tetromino[1].append(L"..X..XX...X.....");
+	tetromino[2].append(L".....XX..XX.....");
+	tetromino[3].append(L"..X..XX..X......");
+	tetromino[4].append(L".X...XX...X.....");
+	tetromino[5].append(L".X...X...XX.....");
+	tetromino[6].append(L"..X...X..XX.....");
 }
